e48056e8e543447fe6ac7c5f7f5e74e0a0625f5d
tinyprocessing <safir@thenoco.co>
2021-01-08 17:12:02 +0300

    notifications

--- Sources/Yelm.Server/user.swift
+++ Sources/Yelm.Server/user.swift
@@ -18,10 +18,20 @@ public class User: ObservableObject, Identifiable {
     public var username: String = ""
     
     
-    public func registration(completionHandlerUser: @escaping (_ success:Bool, _ user: String) -> Void){
+    public func notifications(user: String, token: String){
+        print("your token = " + token)
+        AF.request(ServerAPI.settings.url(method: "user", dev: true), method: .put, parameters: ["login" : user, "push": token]).responseJSON { (response) in
+            
+            if (response.value != nil) {
+                if (ServerAPI.settings.debug){
+                    print(response.value)
+                }
+            }
+        }
         
-        let data_system = ServerAPI.system.data_string()
-        print(data_system)
+    }
+    
+    public func registration(completionHandlerUser: @escaping (_ success:Bool, _ user: String) -> Void){
         
         AF.request(ServerAPI.settings.url(method: "user", dev: true), method: .post, parameters: ["user_info" : ServerAPI.system.data_json()]).responseJSON { (response) in
             
