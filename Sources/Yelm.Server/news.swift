fc530f774ca294d0319233f9c075f656cb9da84e
Michael Safir <admin@yelm.io>
2021-01-20 18:14:31 +0300

    news -> items

--- Sources/Yelm.Server/news.swift
+++ Sources/Yelm.Server/news.swift
@@ -97,4 +97,83 @@ public class News: ObservableObject, Identifiable {
         }
         
     }
+    
+    
+    
+    public func get_news_items(id: Int, completionHandlerItems: @escaping (_ success:Bool,_ objects:[items_structure]) -> Void){
+        var items: [items_structure] = []
+        
+        if (ServerAPI.settings.internet()){
+            
+            AF.request(ServerAPI.settings.url(method: "news-item", dev: true)+"&id=\(id)").responseJSON { (response) in
+                if (response.value != nil) {
+                    let json = JSON(response.value!)
+                    
+                    if (json.count == 0) {
+                        DispatchQueue.main.async {
+                            completionHandlerItems(false, [])
+                        }
+                        return
+                    }
+                    
+                    
+                    
+                    let object = json
+                    print(object)
+                    
+                    
+                    for j in 0...object.count - 1  {
+                        let item_AF = object[j]
+//                        math discount
+                        let price_AF = Float(item_AF["discount"].int!) / 100
+                        let discount_AF = item_AF["price"].float! * price_AF
+                        let discount_final = item_AF["price"].float! - discount_AF
+                        let final = discount_final
+                        
+                        let parameter_AF = item_AF["specification"]
+                        var parameters : [parameters_structure] = []
+                        
+                        if (parameter_AF.count > 0){
+                            for k in 0...parameter_AF.count - 1 {
+                                let parameter_single = parameter_AF[k]
+                                let name = parameter_single["name"].string!
+                                let value = parameter_single["value"].string!
+                                parameters.append(parameters_structure(id: item_AF["id"].int!, name: name, value: value))
+                            }
+                        }
+                       
+                        
+                        
+                        
+//                        add all items in list
+                        items.append(items_structure(id: item_AF["id"].int!,
+                                                    title: item_AF["name"].string!,
+                                                    price: String(format:"%.2f", item_AF["price"].float!),
+                                                    text: item_AF["description"].string!,
+                                                    thubnail: item_AF["images"][0].string!,
+                                                    price_float: item_AF["price"].float!,
+                                                    all_images: [],
+                                                    parameters: parameters,
+                                                    type: item_AF["type"].string!,
+                                                    quanity: "\(item_AF["unit_type"].int!)",
+                                                    discount: String(format:"%.2f", final),
+                                                    discount_value: item_AF["discount"].int!,
+                                                    discount_present: "-\(item_AF["discount"].int!)%",
+                                                    ItemRating: item_AF["rating"].int!))
+                        
+                    }
+                    
+                    
+                    DispatchQueue.main.async {
+                        completionHandlerItems(true, items)
+                    }
+
+                    
+                }
+            }
+            
+        }
+        
+    }
+    
 }
