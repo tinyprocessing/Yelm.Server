d872b24a2a8e3c91e9b0647bd3a2a4973ada3962
Michael Safir <admin@yelm.io>
2021-01-25 19:28:19 +0300

    basket

---
+++ Sources/Yelm.Server/basket.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael on 25.01.2021.
+//
+
+import Foundation
+import Alamofire
+import SwiftUI
+import SwiftyJSON
+import SystemConfiguration
+
+
+public class Basket: ObservableObject, Identifiable {
+    public var id: Int = 0
+    
+    public func get_basket(completionHandlerBasket: @escaping (_ success:Bool) -> Void){
+        
+        
+    }
+    
+}
