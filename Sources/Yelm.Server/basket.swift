80ee73641f3e844737381192d14c7000bdc7f740
Michael Safir <admin@yelm.io>
2021-01-26 11:16:56 +0300

    subcategories

--- Sources/Yelm.Server/basket.swift
+++ Sources/Yelm.Server/basket.swift
@@ -21,14 +21,14 @@ public class Basket: ObservableObject, Identifiable {
     ///   - completionHandlerBasket: Handler
     public func get_basket(items: JSON, completionHandlerBasket: @escaping (_ success:Bool) -> Void){
         
-        print(items.rawString()!)
         
         AF.request(ServerAPI.settings.url(method: "basket", dev: true), method: .post, parameters: ["items": items.rawString()!, "shop_id" : ServerAPI.settings.shop_id]).responseJSON { (response) in
       
+        
             if (response.value != nil && response.response?.statusCode == 200) {
                 
                 let json = JSON(response.value!)
-                
+                print(json)
                 
                 if (json.count == 0) {
                     DispatchQueue.main.async {
