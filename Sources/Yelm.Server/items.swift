6bed91ee1f1b6e6652d864b1e75a22d57b4ec883
tinyprocessing <safir@thenoco.co>
2021-01-26 11:16:56 +0300

    subcategories

--- Sources/Yelm.Server/items.swift
+++ Sources/Yelm.Server/items.swift
@@ -315,4 +315,8 @@ public class Items: ObservableObject, Identifiable {
         }
 
     }
+    
+    public func subcategories(id: Int, completionHandlerSubcategories: @escaping (_ success:Bool,_ objects:[items_main_cateroties]) -> Void){
+        
+    }
 }
