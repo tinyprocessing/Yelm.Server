0fea50cebb7b83d869ca63f659fd54e15d19d32a
tinyprocessing <safir@thenoco.co>
2021-02-14 17:23:05 +0300

    promocodes enum

---
+++ Sources/Yelm.Server/struct/enums.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael on 14.02.2021.
+//
+
+import Foundation
+
+
+public enum Promocodes {
+    case nonactive
+    case delivery
+    case full
+    case percent
+}
