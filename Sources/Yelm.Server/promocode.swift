cacce21f722808b0583ad85c25bdc7e63cf93363
tinyprocessing <safir@thenoco.co>
2021-02-14 17:22:53 +0300

    promocode set

--- Sources/Yelm.Server/promocode.swift
+++ Sources/Yelm.Server/promocode.swift
@@ -15,6 +15,32 @@ import SystemConfiguration
 public class Promocode: ObservableObject, Identifiable {
     public var id: Int = 0
     
+    
+    
+    public func set(promo: String = "", completionHandlerPromocode: @escaping (_ success:Bool, _ promocode:promocode_structure) -> Void){
+       
+        
+        var active : promocode_structure = promocode_structure(id: 0, type: .nonactive, value: 0)
+        
+        if (promo == "full"){
+            
+            active = promocode_structure(id: 0, type: .full, value: 7000)
+        }
+        
+        if (promo == "delivery"){
+            active = promocode_structure(id: 0, type: .delivery, value: 100)
+        }
+        
+        if (promo == "percent"){
+            active = promocode_structure(id: 0, type: .percent, value: 100)
+        }
+        
+        DispatchQueue.main.async {
+            completionHandlerPromocode(true, active)
+        }
+        
+        
+    }
  
     public func get(promo: String = "", user: String = ServerAPI.user.username, completionHandlerPromocode: @escaping (_ success:Bool) -> Void) {
         
