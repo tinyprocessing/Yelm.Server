37dfe10e9d70137bb5b4ba20601193fcc19e4f45
Mikhail Safir <admin@yelm.io>
2021-03-10 11:43:35 +0300

    macos version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "Yelm.Server",
     platforms: [
         .iOS(.v13),
-        .watchOS(.v5)
+        .watchOS(.v5),
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
