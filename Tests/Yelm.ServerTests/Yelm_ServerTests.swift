557c48c64abb168bfdc66accef5ce9f8ab6a0e2a
tinyprocessing <safir@thenoco.co>
2021-03-10 12:49:04 +0300

    test remove

--- Tests/Yelm.ServerTests/Yelm_ServerTests.swift
+++ Tests/Yelm.ServerTests/Yelm_ServerTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class Yelm_ServerTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Yelm_Server().text, "Hello, World!")
+
     }
 
     static var allTests = [
