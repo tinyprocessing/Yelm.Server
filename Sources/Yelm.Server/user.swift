14ab9d111079d9cfd7be6171e4a77e22679968be
tinyprocessing <safir@thenoco.co>
2021-06-08 15:47:47 +0300

    update

--- Sources/Yelm.Server/user.swift
+++ Sources/Yelm.Server/user.swift
@@ -16,7 +16,9 @@ import SwiftUI
 public class User: ObservableObject, Identifiable {
     public var id: Int = 0
     public var username: String = ""
+    public var phone: String = ""
     
+
     
     public func notifications(user: String, token: String){
         
@@ -37,15 +39,6 @@ public class User: ObservableObject, Identifiable {
         #if os(iOS)
         AF.request(ServerAPI.settings.url(method: "user", dev: true), method: .post, parameters: ["user_info" : ServerAPI.system.data_string()]).responseJSON { (response) in
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
             if (response.value != nil) {
                 
                 
@@ -54,9 +47,7 @@ public class User: ObservableObject, Identifiable {
                 if (ServerAPI.settings.debug){
                     print(json)
                 }
-                
-                
-                ServerAPI.user.username = json["login"].string!
+
                 
                 DispatchQueue.main.async {
                     completionHandlerUser(true, json["login"].string!)
@@ -67,5 +58,93 @@ public class User: ObservableObject, Identifiable {
         #endif
     }
     
+    
+    public func account_login(phone: String, completionHandlerUser: @escaping (_ success:Bool,
+                                                                               _ user: String,
+                                                                               _ json: JSON) -> Void){
+        
+        self.phone = phone
+        
+        print(ServerAPI.settings.url(method: "auth", dev: false))
+        
+        AF.request(ServerAPI.settings.url(method: "auth", dev: false), method: .post, parameters: ["phone" : self.phone, "login": ServerAPI.user.username]).responseJSON { (response) in
+            
+            if (response.value != nil && response.response?.statusCode == 200) {
+                
+                
+                let json = JSON(response.value!)
+                
+                print(json)
+                
+                
+                DispatchQueue.main.async {
+                    completionHandlerUser(true, json["hash"].string!, json)
+                }
+                
+            }else{
+                DispatchQueue.main.async {
+                    completionHandlerUser(false, "", JSON())
+                }
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    public func account_get_information(completionHandlerUser: @escaping (_ success:Bool,
+                                                                          _ json: JSON) -> Void){
+        
+        AF.request(ServerAPI.settings.url(method: "user", dev: false), method: .get, parameters: ["login": ServerAPI.user.username]).responseJSON { (response) in
+            
+            if (response.value != nil && response.response?.statusCode == 200) {
+                
+                
+                let json = JSON(response.value!)
+                
+                print(json)
+                
+                DispatchQueue.main.async {
+                    completionHandlerUser(true, json)
+                }
+                
+            }else{
+                DispatchQueue.main.async {
+                    completionHandlerUser(false, JSON())
+                }
+            }
+        }
+        
+    }
+    
+    
+    public func account_update(name: String, notification: Bool, completionHandlerUser: @escaping (_ success:Bool) -> Void){
+        
+        
+        
+        
+        
+        AF.request(ServerAPI.settings.url(method: "user-data", dev: false), method: .put, parameters: ["name" : name,
+                                                                                                   "login": ServerAPI.user.username,
+                                                                                                   "notification" : notification]).responseJSON { (response) in
+            
+            if (response.value != nil && response.response?.statusCode == 200) {
+                
+                
+                
+                DispatchQueue.main.async {
+                    completionHandlerUser(true)
+                }
+                
+            }else{
+                DispatchQueue.main.async {
+                    completionHandlerUser(false)
+                }
+            }
+        }
+        
+        
+    }
     
 }
